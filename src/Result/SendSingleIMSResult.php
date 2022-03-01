71f26cafc295a1a10fddc5285bf5ae997c6be557
xiaoxuan6 <1527736751@qq.com>
2022-03-01 18:44:49 +0800

    添加 test workflows

--- src/Result/SendSingleIMSResult.php
+++ src/Result/SendSingleIMSResult.php
@@ -1,8 +1,14 @@
 <?php
-
+/**
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) vinhson <15227736751@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 namespace Vinhson\LaravelEmaySms\Result;
 
 class SendSingleIMSResult extends Result
 {
-
 }
